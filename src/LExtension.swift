1edd1897db1b3136d6969a6d2f5189e186b99e3a
Leo <superartstudio@gmail.com>
2015-03-17 18:05:43 +1100

    updated extension

--- src/LExtension.swift
+++ src/LExtension.swift
@@ -5,7 +5,7 @@ extension MBProgressHUD {
 		hud.detailsLabelFont = UIFont.systemFontOfSize(18)
 		hud.detailsLabelText = title
 		if duration != nil {
-			hud.mode = MBProgressHUDModeText
+			hud.mode = MBProgressHUDMode.Text
 			hud.minShowTime = duration!
 			hud.graceTime = duration!
 			MBProgressHUD.hideAllHUDsForView(view, animated:true)
