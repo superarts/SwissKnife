301398eedc5a984409f36384fbfef626b47d5704
Leo <leo@superarts.org>
2015-05-11 12:29:44 +1000

    added LConst for colors

--- src/LThemeManager.swift
+++ src/LThemeManager.swift
@@ -52,14 +52,14 @@ struct LTheme {
 				auto_first_language = auto
 			}
 			func append(key:String, _ value:String) {
-				if var array = strings[key] {
+				if var array = dictionary[key] {
 					array.append(value)
-					strings[key] = array
+					dictionary[key] = array
 				} else {
 					if auto_first_language {
-						strings[key] = [key, value]
+						dictionary[key] = [key, value]
 					} else {
-						strings[key] = [value]
+						dictionary[key] = [value]
 					}
 				}
 			}
@@ -96,6 +96,8 @@ struct LTheme {
 					return key
 				}
 			}
+			LF.log("x1", key)
+			LF.log("x2", i)
 			if let array = strings[key] {
 				if i < array.count {
 					return array[i!]
@@ -103,197 +105,7 @@ struct LTheme {
 			}
 			return key
 		}
-
-		static func test() {
-			//	default language from system settings
-			LF.log("language", LTheme.localization.language?.rawValue)
-			LF.log("test 1", LTheme.localization.string("yes"))
-			LF.log("test 2", LTheme.localization.string("na"))
-
-			//	set language to a supported language
-			LTheme.localization.language = .SpanishMexico
-			LF.log("")
-			LF.log("test 1", LTheme.localization.string("yes"))
-			LF.log("test 2", LTheme.localization.string("na"))
-
-			//	set language to a language that is not supported (default language is english)
-			LTheme.localization.language = .French
-			LF.log("")
-			LF.log("test 1", LTheme.localization.string("yes"))
-			LF.log("test 2", LTheme.localization.string("na"))
-
-			//	no default language - use default language as fallback
-			LTheme.localization.language = nil
-			LF.log("")
-			LF.log("test 1", LTheme.localization.string("yes"))
-			LF.log("test 2", LTheme.localization.string("na"))
-
-			//	disable default language - keys are used instead
-			LTheme.localization.language_default = nil
-			LF.log("")
-			LF.log("test 1", LTheme.localization.string("yes"))
-			LF.log("test 2", LTheme.localization.string("na"))
-
-			//	set language index manually
-			LF.log("")
-			LF.log("test 1", LTheme.localization.string("yes", index:2))
-			LF.log("test 2", LTheme.localization.string("na", index:999))
-
-			//	define a new string pack manually
-			var set0 = [
-				"good": [
-					"Good",
-					"好",
-					"Bien",
-				],
-				"bad": [
-					"Bad",
-					"坏",
-					"Mal",
-				],
-			]
-			//	or use helper class
-
-			//	split by meanings, good for translators who know different languages
-			var set1 = LTheme.localization.StringPack()
-			set1.append("big", "Big")
-			set1.append("big", "大")
-			set1.append("big", "Grande")
-			set1.append("small", "Small")
-			set1.append("small", "小")
-			set1.append("small", "Pequeño")
-
-			//	split by languages, good for translators to work separately
-			var set2 = LTheme.localization.StringPack()
-			set2.append("new", "New")
-			set2.append("old", "Old")
-			set2.append("new", "新")
-			set2.append("old", "旧")
-			set2.append("new", "Nuevo")
-			set2.append("old", "Viejo")
-
-			//	load new string sets
-			LTheme.localization.strings_append(set0)
-			LTheme.localization.strings_append(set1.dictionary)
-			LTheme.localization.strings_append(set2.dictionary)
-			LTheme.localization.language_reload()
-			LF.log("")
-			LF.log("test 1", LTheme.localization.string("good"))
-			LF.log("test 2", LTheme.localization.string("bad"))
-			LF.log("test 3", LTheme.localization.string("big"))
-			LF.log("test 4", LTheme.localization.string("small"))
-			LF.log("test 5", LTheme.localization.string("new"))
-			LF.log("test 6", LTheme.localization.string("old"))
-			LF.log("test x", LTheme.localization.string("wow"))
-		}
 	}
-}
-
-struct LColor {
-	//	win8: sidebar-bg, highlighted-bg, main-bg, tile-bg - from http://www.creepyed.com/2012/09/windows-8-colors-hex-code/
-	static let win8start: [[UIColor]] = [
-		[
-			UIColor(rgb:0x2E1700),
-			UIColor(rgb:0x632F00),
-			UIColor(rgb:0x261300),
-			UIColor(rgb:0x543A24),
-		], [
-			UIColor(rgb:0x2E1700),
-			UIColor(rgb:0x632F00),
-			UIColor(rgb:0x261300),
-			UIColor(rgb:0x543A24),
-		], [
-			UIColor(rgb:0x4E0000),
-			UIColor(rgb:0xB01E00),
-			UIColor(rgb:0x380000),
-			UIColor(rgb:0x61292B),
-		], [
-			UIColor(rgb:0x4E0038),
-			UIColor(rgb:0xC1004F),
-			UIColor(rgb:0x40002E),
-			UIColor(rgb:0x662C58),
-		], [
-			UIColor(rgb:0x2D004E),
-			UIColor(rgb:0x7200AC),
-			UIColor(rgb:0x250040),
-			UIColor(rgb:0x4C2C66),
-		], [
-			UIColor(rgb:0x1F0068),
-			UIColor(rgb:0x4617B4),
-			UIColor(rgb:0x180052),
-			UIColor(rgb:0x423173),
-		], [
-			UIColor(rgb:0x001E4E),
-			UIColor(rgb:0x006AC1),
-			UIColor(rgb:0x001940),
-			UIColor(rgb:0x2C4566),
-		], [
-			UIColor(rgb:0x004D60),
-			UIColor(rgb:0x008287),
-			UIColor(rgb:0x004050),
-			UIColor(rgb:0x306772),
-		], [
-			UIColor(rgb:0x004A00),
-			UIColor(rgb:0x199900),
-			UIColor(rgb:0x003E00),
-			UIColor(rgb:0x2D652B),
-		], [
-			UIColor(rgb:0x15992A),
-			UIColor(rgb:0x00C13F),
-			UIColor(rgb:0x128425),
-			UIColor(rgb:0x3A9548),
-		], [
-			UIColor(rgb:0xE56C19),
-			UIColor(rgb:0xFF981D),
-			UIColor(rgb:0xC35D15),
-			UIColor(rgb:0xC27D4F),
-		], [
-			UIColor(rgb:0xB81B1B),
-			UIColor(rgb:0xFF2E12),
-			UIColor(rgb:0x9E1716),
-			UIColor(rgb:0xAA4344),
-		], [
-			UIColor(rgb:0xB81B6C),
-			UIColor(rgb:0xFF1D77),
-			UIColor(rgb:0x9E165B),
-			UIColor(rgb:0xAA4379),
-		], [
-			UIColor(rgb:0x691BB8),
-			UIColor(rgb:0xAA40FF),
-			UIColor(rgb:0x57169A),
-			UIColor(rgb:0x7F6E94),
-		], [
-			UIColor(rgb:0x1B58B8),
-			UIColor(rgb:0x1FAEFF),
-			UIColor(rgb:0x16499A),
-			UIColor(rgb:0x6E7E94),
-		], [
-			UIColor(rgb:0x569CE3),
-			UIColor(rgb:0x56C5FF),
-			UIColor(rgb:0x4294DE),
-			UIColor(rgb:0x6BA5E7),
-		], [
-			UIColor(rgb:0x00AAAA),
-			UIColor(rgb:0x00D8CC),
-			UIColor(rgb:0x008E8E),
-			UIColor(rgb:0x439D9A),
-		], [
-			UIColor(rgb:0x83BA1F),
-			UIColor(rgb:0x91D100),
-			UIColor(rgb:0x7BAD18),
-			UIColor(rgb:0x94BD4A),
-		], [
-			UIColor(rgb:0xD39D09),
-			UIColor(rgb:0xE1B700),
-			UIColor(rgb:0xC69408),
-			UIColor(rgb:0xCEA539),
-		], [
-			UIColor(rgb:0xE064B7),
-			UIColor(rgb:0xFF76BC),
-			UIColor(rgb:0xDE4AAD),
-			UIColor(rgb:0xE773BD),
-		], 
-	]
 }
 
 //	UIKit + Interface Builder: define various UIView properties in IB
